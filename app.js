45ccb456d63b87ecd7d66ffbe61d3d9f0ee0cc58
Lee Chimfwembe <49544669+201800735@users.noreply.github.com>
2020-09-13 01:07:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.use(cors())
 app.use(bodyParser.json())
 
 if(process.env.NODE_ENV==='production'){
-	app.use(express.static(__dirname+"/build"))
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname+"/build/index.html"))
+	app.use(express.static(__dirname+"/public"))
+	app.get("/", (req, res) => {
+		res.sendFile(path.join(__dirname+"/public/index.html"))
 	})
 }
 app.set('port', (process.env.PORT || 3000))
